9f60c30a1c3733043ec1e428326fa4f05ea53829
CozyGollyGlow <cozygollyglow@protonmail.com>
2026-02-20 02:08:14 +0100

    clicking behavior

--- script.js
+++ script.js
@@ -13,6 +13,17 @@ function addImages(){
 			const img = document.createElement("img");
 			img.src = src;
 
+			img.addEventListener("click", (e) => {
+				if (e.target.tagName === "IMG") {
+			    	try {
+						await navigator.clipboard.writeText(e.target.src);
+				    	console.log("Copied!");
+				    } catch (err) {
+				    	console.error("Copy failed:", err);
+				    }
+			  	}
+			});
+
 			container.appendChild(img)
       		grid.appendChild(container);
     	});
